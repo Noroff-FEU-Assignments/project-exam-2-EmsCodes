fd168651a2f4b3ab82f89d5a3662114fb1006cb6
Endre Makra-Stenkløv <69721898+EmsCodes@users.noreply.github.com>
2022-04-19 15:00:00 +0200

    .

--- components/utils/buttons/Cta.js
+++ components/utils/buttons/Cta.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "../../../styles/components/utils/buttons/Cta.module.css";
 
 function Cta({ content }) {
   return <button className="main-cta">{content}</button>;
