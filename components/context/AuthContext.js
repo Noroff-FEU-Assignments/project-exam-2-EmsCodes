90c477aa723734f8dd18465207d882c1d6f2beb5
Endre Makra-Stenkløv <69721898+EmsCodes@users.noreply.github.com>
2022-04-19 10:38:11 +0200

    installed next.js

---
+++ components/context/AuthContext.js
@@ -0,0 +1,16 @@
+import React from "react";
+import useLocalStorage from "../../hooks/UseLocalStorage";
+
+export const AuthContext = React.createContext([null, () => {}]);
+
+function AuthProvider({ children }) {
+  const [auth, setAuth] = useLocalStorage("auth", null);
+
+  return (
+    <AuthContext.Provider value={[auth, setAuth]}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthProvider;
