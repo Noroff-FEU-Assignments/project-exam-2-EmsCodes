35b72ef2f323e3bc12698bb5ccee469aa431c2cc
Endre Makra-Stenkløv <69721898+EmsCodes@users.noreply.github.com>
2022-04-21 14:52:48 +0200

    added propTypes to components

---
+++ pages/contact.js
@@ -0,0 +1,9 @@
+import HeroSection from "../components/utils/global/HeroSection";
+import Head from "../components/head/Head";
+import Heading from "../components/utils/heading/Heading";
+
+function contact() {
+  return <div>contact</div>;
+}
+
+export default contact;
