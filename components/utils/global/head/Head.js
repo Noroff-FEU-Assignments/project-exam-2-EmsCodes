1d6381aeef4c9a5a0dd7ec8a8b1fbfab37e75361
Endre Makra-Stenkløv <69721898+EmsCodes@users.noreply.github.com>
2022-05-26 23:40:05 +0200

    .

--- components/utils/global/head/Head.js
+++ components/utils/global/head/Head.js
@@ -8,6 +8,8 @@ function Head({ title = "" }) {
         {title}
         {title ? " | " : ""} Holidaze
       </title>
+
+      <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="author" content="Endre Makra-Stenkløv" />
       <meta
